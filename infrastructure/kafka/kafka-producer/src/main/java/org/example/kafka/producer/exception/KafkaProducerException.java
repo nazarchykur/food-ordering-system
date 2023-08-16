e500764c4d28d36dbe89e27d229122abf26d53c2
nazarchykur <nazarchykur@gmail.com>
2023-08-16 13:32:19 +0300

    Implemented Kafka producer generic module

---
+++ infrastructure/kafka/kafka-producer/src/main/java/org/example/kafka/producer/exception/KafkaProducerException.java
@@ -0,0 +1,8 @@
+package org.example.kafka.producer.exception;
+
+public class KafkaProducerException extends RuntimeException {
+
+    public KafkaProducerException(String message) {
+        super(message);
+    }
+}
